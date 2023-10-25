1fcbd2ae8a5b88075334cbe420ee85f457761441
alearanc <alexisarancibia97@gmail.com>
2023-10-25 16:41:35 -0300

    ejercicio-02.js added

---
+++ js/ejercicio-02.js
@@ -0,0 +1,16 @@
+//  Implementación propia de la función find()
+function myFind(arr, func) {
+    for (let i = 0; i < arr.length; i++) {
+        //  si el elemento cumple con la función, lo retorna
+        if (func(arr[i], i)) {
+            return arr[i]
+        }
+    }
+}
+
+const arr = [1, 77, 24, 52, -14],
+    arr1 = ['chau', 'hola', ':D']
+
+console.log(myFind(arr, (el) => el > 15)) //  77
+console.log(myFind(arr, (el) => el > 15 && el < 50)) //  24
+console.log(myFind(arr1, (el) => el == ':D')) //   :D
