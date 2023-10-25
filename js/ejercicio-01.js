9d7a14d514903d66fd6526eccb372c294670803f
alearanc <alexisarancibia97@gmail.com>
2023-10-25 17:23:36 -0300

    code cleanup

--- js/ejercicio-01.js
+++ js/ejercicio-01.js
@@ -1,17 +1,17 @@
 //  Implementación de función map() propia
 function myMap(arr, func) {
-    let result = [];
+    let result = []
     for (let i = 0; i < arr.length; i++) {
-        result.push(func(arr[i]));
+        result.push(func(arr[i]))
     }
     return result
 }
 
 //  Otra manera utilizando forEach()
 function myMap2(arr, func) {
-    let result = [];
+    let result = []
     arr.forEach((i) => {
-        result.push(func(i));
+        result.push(func(i))
     })
     return result
 }
@@ -21,5 +21,5 @@ let arr1 = [4, 9, 16, 25],
     arr2 = myMap2(arr1, Math.sqrt),
     arr3 = myMap2(['1', '2', 'hola', '7'], (a) => a + a)
 
-console.log(arr2)   // [2, 3, 4, 5]
-console.log(arr3)   // ['11', '22', 'holahola', '77']
+console.log(arr2) // [2, 3, 4, 5]
+console.log(arr3) // ['11', '22', 'holahola', '77']
